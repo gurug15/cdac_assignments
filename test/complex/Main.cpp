f6873ea8fdccf8b2631f117e64bfbc64657d91af
Guruprasad Gastgar <gurugastgar@gmail.com>
2025-03-09 01:29:54 +0530

    edited main

--- test/complex/Main.cpp
+++ test/complex/Main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 
 int main(){
-    Complex c1(90,30), c2(30,40), c4(50,40);
+    Complex c1(90,30), c2(30,40), c4(50,40) ,c5(50,0);
     //c1.Accept();
     c1.Display();
     
-    Complex  c3 = c1 + c2 -c4;
+    Complex  c3 = c1 + c2 - c4 + c5;
     c3.Display();
     return 0;
 }
