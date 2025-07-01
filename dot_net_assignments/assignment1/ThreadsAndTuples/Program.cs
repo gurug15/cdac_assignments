f5ddbc8e6aee597aab6886ef9cbfc009c38048bd
GURUGASTGAR <gurugastgar@gmail.com>
2025-07-02 02:27:00 +0530

    learingng partial classes, linq, and more

---
+++ dot_net_assignments/assignment1/ThreadsAndTuples/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ThreadsAndTuples
+{
+    internal class Program
+    {
+        static void Func1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+
+                Console.WriteLine("Func1---" + i);
+            }
+            
+        }
+
+        static void Func2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+
+                Console.WriteLine("Func2---" + i);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(Func1));
+            t1.Start();
+            Thread t2 = new Thread(new ThreadStart(Func2));
+
+            t1.Join();
+            Console.WriteLine("main thred");
+            t2.Start();
+            Console.WriteLine("main thred");
+
+
+            
+
+            void func1(out int x, out int y)
+            {
+                x = -1;
+                y = -1;
+            }
+            int x, y;
+            func1(out x,out y);
+            Console.WriteLine($"{x}, : : {y}");
+        }
+    }
+}
